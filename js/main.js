e38550dc9843d16caef1141d216673db2691c0bf
bbb324 <xiejun325@gmail.com>
2017-07-17 16:24:50 +0800

    画个球，画方块，设置平面

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 (function() {
   var scene = new THREE.Scene();
   var camera = function() {
-    let view_angle = 30,
+    let view_angle = 45,
         aspect = window.innerWidth / window.innerHeight,
-        near = 1,
+        near = 0.1,
         far = 1000;
     return (new THREE.PerspectiveCamera(
         view_angle,
@@ -13,24 +13,48 @@
       ));
   }
   this.camera = new camera();
-  this.camera.position.set(0,0,5);
+  this.camera.position.set(-30, 40, 30);
+  this.camera.lookAt(scene.position);
   
 
   var renderer = new THREE.WebGLRenderer();
   //renderer.setClearColor('red');
+   renderer.setClearColor(new THREE.Color(0xEEEEEE));
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
+  // show x,y axes
+  var axes = new THREE.AxisHelper(20);
+  scene.add(axes);
 
-  // define cubic
+  //设置地面
+  var plane = function() {
+    this.planeGeo = new THREE.PlaneGeometry(60, 20);
+    this.planeMat = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    return new THREE.Mesh(this.planeGeo, this.planeMat);
+  }
+  //设置地面角度方便查看
+  this.plane = new plane();
+  this.plane.rotation.x = -0.5*Math.PI;
+  this.plane.position.x = 15;
+  this.plane.position.y = 0;
+  this.plane.position.z = 0;
+  scene.add(this.plane);
+
+
+  // 画个方块
   var cubic = function() {
-    this.geometry = new THREE.BoxGeometry(1, 1, 1);
-    this.material = new THREE.MeshBasicMaterial({
-      color: 0x00ff00
+    this.geometry = new THREE.BoxGeometry(10, 10, 10);
+    this.material = new THREE.MeshLambertMaterial({
+      color: 0xF3FFE2,
+      wireframe: true
     });
     return new THREE.Mesh(this.geometry, this.material);
   }
+
   this.cubic = new cubic();
+  this.cubic.position.y = 5;
+  scene.add(this.cubic);
 
   // define line
   var line = function() {
@@ -43,20 +67,39 @@
     line_geometry.vertices.push(new THREE.Vector3(10, 0, 0));
     return new THREE.Line(line_geometry, lineStyle);
   }
-  
-  //scene.add(new line());
-  scene.add(this.cubic);
 
+  //画个球
+  var sphere = function() {
+    var obj = new THREE.SphereGeometry(4,20,20);
+    var meterial = new THREE.MeshBasicMaterial({ color: 0x999999, wireframe:true } );
+    return new THREE.Mesh(obj, meterial);
+  }
+  
+  this.sphere = new sphere();
+  this.sphere.position.x = 30;
+  this.sphere.position.y = 10;
+  scene.add(this.sphere);
   
 
   var animate = function() {
     requestAnimationFrame(animate);
-     this.cubic.rotation.x+=0.2;
-
-   
+    /*this.sphere.rotation.x+=0.02;
+    this.sphere.rotation.y+=0.02;*/
+    
     renderer.render(scene, this.camera);
   };
 
   animate();
+
 })();
+
+
+
+
+
+
+
+
+
+
 
